89a8b5b278082ec374f1ade381cf1f06a43a89e2
Berkant <47941900+berkanttelli@users.noreply.github.com>
2021-06-15 11:47:49 +0300

    Kucuk Duzeltmeler

--- Assets/Scripts/PuckScript.cs
+++ Assets/Scripts/PuckScript.cs
@@ -7,7 +7,8 @@ public class PuckScript : MonoBehaviour
     public Collider2D border;
     Vector2 pos;
     Rigidbody2D rb;
-    List<Collision2D> collisions = new List<Collision2D>();
+    // private ve readonly anahtar kelimeleri eklendi.
+    private readonly List<Collision2D> collisions = new List<Collision2D>();
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
@@ -17,13 +18,15 @@ public class PuckScript : MonoBehaviour
     {
         if (collision.Equals(border))
         {
+            // Burada bir zamanlar if vardi.
             pos = transform.position;
             pos.x = Mathf.Clamp(pos.x, border.bounds.min.x, border.bounds.max.x);
             pos.y = Mathf.Clamp(pos.y, border.bounds.min.y, border.bounds.max.y);
             rb.MovePosition(pos);
             if(collisions.Count != 0)
-                rb.velocity = Vector2.Reflect(rb.velocity, collisions[0].contacts[0].normal);
+                rb.velocity = Vector2.Reflect(rb.velocity, collisions[0].GetContact(0).normal);
             else rb.velocity = Vector2.Reflect(rb.velocity, Vector2.up);
+            
         }
     }
 
