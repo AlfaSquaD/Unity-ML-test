ec966bbacd02824f1b61f382316736b0ac9268c7
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-06-15 15:55:58 +0300

    Added agent

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
 public class PlayerAgent : Agent
 {
@@ -11,11 +12,33 @@ public class PlayerAgent : Agent
     [SerializeField] private Collider2D ownGoal;
     [SerializeField] private Collider2D enemyGoal;
     [SerializeField] private Transform enemyTransform;
+    private PlayerMovement pm;
 
+    private void Start()
+    {
+        pm = gameObject.GetComponent<PlayerMovement>();
+    }
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(ballTransform.localPosition);
         sensor.AddObservation(gameObject.transform.localPosition);
         sensor.AddObservation(enemyTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        Debug.Log(actions.ContinuousActions);
+        pm.movePlayerAgent(new Vector2(actions.ContinuousActions[0], actions.ContinuousActions[1]));
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            continousActions[0] = mousePos.x;
+            continousActions[1] = mousePos.y;
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,30 +17,48 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
         if (Input.GetMouseButton(0))
         {
-            if (wasJustClicked)
-            {
-                wasJustClicked = false;
-                canMove = playArea.Equals(hit.collider);
-            }
-
-            if (canMove)
-            {
-                Vector2 pos = transform.position;
-                Vector2 nPos = pos + ((mousePos - pos) * Time.deltaTime * maxSpeed);
-                if (nPos.x > playArea.bounds.max.x || nPos.x < playArea.bounds.min.x || nPos.y > playArea.bounds.max.y || nPos.y < playArea.bounds.min.y)
-                {
-                    nPos.x = Mathf.Clamp(nPos.x, playArea.bounds.min.x, playArea.bounds.max.x);
-                    nPos.y = Mathf.Clamp(nPos.y, playArea.bounds.min.y, playArea.bounds.max.y);
-                }
-
-                rb.MovePosition(nPos);
-            }
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            movePlayer(mousePos);
         }
         else wasJustClicked = true;
+    }
+
+    public void movePlayer(Vector2 mousePos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+        if (wasJustClicked)
+        {
+            wasJustClicked = false;
+            canMove = playArea.Equals(hit.collider);
+        }
+
+        if (canMove)
+        {
+            Vector2 pos = transform.position;
+            Vector2 nPos = pos + ((mousePos - pos) * Time.deltaTime * maxSpeed);
+            if (nPos.x > playArea.bounds.max.x || nPos.x < playArea.bounds.min.x || nPos.y > playArea.bounds.max.y || nPos.y < playArea.bounds.min.y)
+            {
+                nPos.x = Mathf.Clamp(nPos.x, playArea.bounds.min.x, playArea.bounds.max.x);
+                nPos.y = Mathf.Clamp(nPos.y, playArea.bounds.min.y, playArea.bounds.max.y);
+            }
+
+            rb.MovePosition(nPos);
+        }   
+    }
+
+    public void movePlayerAgent(Vector2 mousePos)
+    {
+        Vector2 pos = transform.position;
+        Vector2 nPos = pos + ((mousePos - pos) * Time.deltaTime * maxSpeed);
+        if (nPos.x > playArea.bounds.max.x || nPos.x < playArea.bounds.min.x || nPos.y > playArea.bounds.max.y || nPos.y < playArea.bounds.min.y)
+        {
+            nPos.x = Mathf.Clamp(nPos.x, playArea.bounds.min.x, playArea.bounds.max.x);
+            nPos.y = Mathf.Clamp(nPos.y, playArea.bounds.min.y, playArea.bounds.max.y);
+        }
+
+        rb.MovePosition(nPos);
     }
 
 }
